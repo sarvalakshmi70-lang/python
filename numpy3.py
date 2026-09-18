732223e25c4edca1fa15844c5ae39bd5c349cedd
sarva <sarvalakshmi70@gmail.com>
2026-09-18 19:50:23 +0530

    task3

---
+++ numpy3.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+zeros = np.zeros(5)
+ones = np.ones(5)
+full = np.full(5, 10)
+range_numbers = np.arange(1, 6)
+line_numbers = np.linspace(0, 10, 5)
+random_numbers = np.random.randint(1, 100, 5)
+
+print("Zeros:")
+print(zeros)
+
+print("Ones:")
+print(ones)
+
+print("Full:")
+print(full)
+
+print("Arange:")
+print(range_numbers)
+
+print("Linspace:")
+print(line_numbers)
+
+print("Random Numbers:")
+print(random_numbers)
