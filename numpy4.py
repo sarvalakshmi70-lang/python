e95255e59be4b335f556974ebe89f11082512e55
sarva <sarvalakshmi70@gmail.com>
2026-09-19 20:28:56 +0530

    task4

---
+++ numpy4.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("Addition:")
+print(numbers + 5)
+
+print("Subtraction:")
+print(numbers - 5)
+
+print("Multiplication:")
+print(numbers * 2)
+
+print("Division:")
+print(numbers / 2)
+
+print("Greater than 25:")
+print(numbers > 25)
+
+print("Total:")
+print(np.sum(numbers))
+
+print("Average:")
+print(np.mean(numbers))
