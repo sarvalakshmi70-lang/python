3e7185a07b61c5792d72067ab58ad6678e2651f3
sarva <sarvalakshmi70@gmail.com>
2026-09-22 20:28:23 +0530

    pandas task2

---
+++ pandas2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data = {
+    "Name": ["Sarva", "gayathri", "Vimal", "Velan", "Vishnu"],
+    "Age": [20, 21, 20, 22, 21],
+    "Mark": [85, 90, 78, 88, 95]
+}
+
+df = pd.DataFrame(data)
+
+print(df["Name"])
+
+print(df[["Name", "Mark"]])
+
+print(df.loc[0])
+
+print(df.iloc[1])
+
+print(df[df["Mark"] > 80])
+
+print(df[(df["Age"] == 21) & (df["Mark"] > 80)])
+
+print(df[df["Name"].isin(["Arun", "Meena"])])
